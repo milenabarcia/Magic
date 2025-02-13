cc2ecbe49dd3e590a4958e09a9900dced00c2e66
milenabarcia <mimi_barciahotmail.com>
2025-02-12 22:33:11 -0300

    aula

---
+++ src/app/menu-superior/menu-superior.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-superior',
+  imports: [],
+  templateUrl: './menu-superior.component.html',
+  styleUrl: './menu-superior.component.css'
+})
+export class MenuSuperiorComponent {
+
+}
